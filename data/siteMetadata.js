a8e08829d30c89bf1f4749939537647fbe1f81d9
Kane <kane@KanedeMacBook-Pro.local>
2023-08-11 01:32:42 +0800

    first commit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,21 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Know2note',
+  author: 'Jian An Lin',
+  headerTitle: 'Know2note',
+  description: 'A blog for sharing knowledge and notes about programming and more.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://blog.jianan1104.dev',
+  siteRepo: 'https://github.com/jianan1104/blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'linjianan1104@gmail.com',
+  github: 'https://github.com/jianan1104',
+  twitter: 'https://twitter.com/linjianan_1104',
+  facebook: 'https://www.facebook.com/Linjianan1104/',
+  linkedin: 'https://www.linkedin.com/in/jiananlin1104/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
@@ -40,7 +39,7 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: 'emailoctopus',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
